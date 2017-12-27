32d246e55e928cebd56dccf3e647e270181703cc
Balaji <balaji.x.selvarajan@gmail.com>
2017-12-27 22:28:12 +0530

    commit from eclipse

--- learn/Test.java
+++ learn/Test.java
@@ -3,6 +3,6 @@ class Test
 public static void main(String args[])
 {
 System.out.println("This is sample program");
-System.out.println("another test");
+System.out.println("another test - hello");
 }
 }
